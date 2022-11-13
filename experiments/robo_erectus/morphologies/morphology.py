2cdbe6a4ac73b831999d2588988be47051b8c0bb
Dingkang <1205498974@qq.com>
2022-11-13 15:54:36 +0100

    Add fix-morphology creator and test files

---
+++ experiments/robo_erectus/morphologies/morphology.py
@@ -0,0 +1,94 @@
+"""
+Fixed morphology creator
+refer to ci-group/revolve/experiments/examples/yaml & revolve/pyrevolve/revolve_bot/revolve_bot.py 
+"""
+import yaml
+import math
+from revolve2.core.modular_robot import ActiveHinge, Body, Core, Brick
+
+
+class FixedBodyCreator:
+    def __init__(self, yaml_file):
+        self.yaml_file = yaml_file
+        self.load_file(yaml_file)
+
+    def load_file(self, path, conf_type="yaml"):
+        """
+        Read robot's description from a file and parse it to Python structure
+        :param path: Robot's description file path
+        :param conf_type: Type of a robot's description format
+        :return:
+        """
+        with open(path, "r") as robot_file:
+            text = robot_file.read()
+
+        if "yaml" == conf_type:
+            self.load_yaml(text)
+        elif "sdf" == conf_type:
+            raise NotImplementedError("Loading from SDF not yet implemented")
+
+    def load_yaml(self, text):
+        """
+        Load robot's description from a yaml string
+        :param text: Robot's yaml description
+        """
+        yaml_bot = yaml.safe_load(text)
+        self._id = yaml_bot["id"] if "id" in yaml_bot else None
+        self._core = self.FromYaml(yaml_bot["body"])
+        self._body = Body()
+        self._body.core = self._core
+        self._body.finalize()
+
+    def FromYaml(self, yaml_object):
+        """
+        From a yaml object, creates a data struture of interconnected body modules.
+        Standard names for modules are:
+        Core
+        ActiveHinge
+        Brick
+        """
+        mod_type = yaml_object["type"]
+        if mod_type == "CoreComponent" or mod_type == "Core":
+            module = Core(0.0)
+        elif mod_type == "ActiveHinge":
+            module = ActiveHinge(math.pi / 2.0)
+        elif mod_type == "Brick":
+            module = Brick(0.0)
+        else:
+            raise NotImplementedError(
+                '"{}" module not yet implemented'.format(mod_type)
+            )
+
+        # module.id = yaml_object['id']
+
+        try:
+            module.orientation = yaml_object["orientation"]
+        except KeyError:
+            module.orientation = 0
+
+        try:
+            module.rgb = (
+                yaml_object["params"]["red"],
+                yaml_object["params"]["green"],
+                yaml_object["params"]["blue"],
+            )
+        except KeyError:
+            pass
+
+        if "children" in yaml_object:
+            for parent_slot in yaml_object["children"]:
+                module.children[parent_slot] = self.FromYaml(
+                    yaml_object=yaml_object["children"][parent_slot]
+                )
+
+        return module
+
+    @property
+    def body(self):
+        return self._body
+
+
+if __name__ == "__main__":
+    path = "/home/dingkang/codes/revolve2/experiments/robo_erectus/morphologies/spider.yaml"
+    eretus = FixedBodyCreator(path)
+    body = eretus.body
---
+++ experiments/robo_erectus/morphologies/prototype.py
@@ -0,0 +1,108 @@
+"""Visualize and run a modular robot using Mujoco."""
+
+import math
+from random import Random
+
+from pyrr import Quaternion, Vector3
+from revolve2.actor_controller import ActorController
+from revolve2.core.modular_robot import ActiveHinge, Body, Brick, ModularRobot
+from revolve2.core.modular_robot.brains import BrainCpgNetworkNeighbourRandom
+from revolve2.core.physics.running import ActorControl, Batch, Environment, PosedActor
+from revolve2.runners.mujoco import LocalRunner
+
+
+class Simulator:
+    """
+    Simulator setup.
+
+    Simulates using Mujoco.
+    Defines a control function that steps the controller and applies the degrees of freedom the controller provides.
+    """
+
+    _controller: ActorController
+
+    async def simulate(self, robot: ModularRobot, control_frequency: float) -> None:
+        """
+        Simulate a robot.
+
+        :param robot: The robot to simulate.
+        :param control_frequency: Control frequency for the simulator.
+        """
+        batch = Batch(
+            simulation_time=1000000,
+            sampling_frequency=0.0001,
+            control_frequency=control_frequency,
+            control=self._control,
+        )
+
+        actor, self._controller = robot.make_actor_and_controller()
+        bounding_box = actor.calc_aabb()
+
+        env = Environment()
+        env.actors.append(
+            PosedActor(
+                actor,
+                Vector3(
+                    [
+                        0.0,
+                        0.0,
+                        bounding_box.size.z / 2.0 - bounding_box.offset.z,
+                    ]
+                ),
+                Quaternion(),
+                [0.0 for _ in self._controller.get_dof_targets()],
+            )
+        )
+        batch.environments.append(env)
+
+        runner = LocalRunner()
+        await runner.run_batch(batch)
+
+    def _control(
+        self, environment_index: int, dt: float, control: ActorControl
+    ) -> None:
+        self._controller.step(dt)
+        control.set_dof_targets(0, self._controller.get_dof_targets())
+
+
+async def main() -> None:
+    """Run the simulation."""
+    rng = Random()
+    rng.seed(5)
+
+    body = Body()
+    body.core.back = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment = Brick(0.0)
+
+    body.core.back.attachment.attachment.left = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.left.attachment = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.left.attachment.attachment = Brick(0.0)
+    body.core.back.attachment.attachment.left.attachment.attachment.left = Brick(0.0)
+    body.core.back.attachment.attachment.left.attachment.attachment.left.front = Brick(0.0)
+    body.core.back.attachment.attachment.left.attachment.attachment.left.front.front = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.left.attachment.attachment.left.front.front.attachment = Brick(0.0)
+    body.core.back.attachment.attachment.left.attachment.attachment.left.front.front.attachment.front = Brick(0.0)
+
+    body.core.back.attachment.attachment.right = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.right.attachment = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.right.attachment.attachment = Brick(0.0)
+    body.core.back.attachment.attachment.right.attachment.attachment.right = Brick(0.0)
+    body.core.back.attachment.attachment.right.attachment.attachment.right.front = Brick(0.0)
+    body.core.back.attachment.attachment.right.attachment.attachment.right.front.front = ActiveHinge(math.pi / 2.0)
+    body.core.back.attachment.attachment.right.attachment.attachment.right.front.front.attachment = Brick(0.0)
+    body.core.back.attachment.attachment.right.attachment.attachment.right.front.front.attachment.front = Brick(0.0)
+
+    body.finalize()
+
+    brain = BrainCpgNetworkNeighbourRandom(rng)
+    robot = ModularRobot(body, brain)
+
+    sim = Simulator()
+    await sim.simulate(robot, 60)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
